087d32334a7f8a36a7384ee6d6d52f7a77173787
mmbensalah <mmbensalah@gmail.com>
2018-08-31 18:22:18 -0600

    TESTS visitor can create a login

---
+++ spec/features/user/visitor_can_create_a_login_spec.rb
@@ -0,0 +1,19 @@
+require 'rails_helper'
+
+describe 'Visitor can' do
+  it 'create a sign in' do
+    visit '/'
+
+    click_on 'Sign Up To Be a User'
+
+    expect(current_path).to eq new_user_path
+
+    fill_in :user_username, with: 'user_1'
+    fill_in :user_password, with: 'test_1'
+
+    click_on "Create User"
+
+    expect(page).to have_content("Welcome #{username}!")
+
+  end
+end
