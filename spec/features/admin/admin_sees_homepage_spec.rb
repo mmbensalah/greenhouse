630c5aebe8692700b458bf33494a297b7350781f
mmbensalah <mmbensalah@gmail.com>
2018-09-03 14:27:21 -0600

    TESTS and IMPLEMENTS admin vs user functionality

--- spec/features/admin/admin_sees_homepage_spec.rb
+++ spec/features/admin/admin_sees_homepage_spec.rb
@@ -7,7 +7,21 @@ describe "User visits admin homepage" do
 
       allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(admin)
 
-      visit admin_homepage_path
+      visit admin_homepage_index_path
+      expect(page).to have_content('Administrator Homepage')
+    end
+  end
+  context 'as default user' do
+    it 'does not allow default user to see admin homepage index' do
+      user = User.create(username: 'penelope', password: '1234', role: 0)
+
+      allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user)
+
+      visit admin_homepage_index_path
+
+      expect(page).to_not have_content('Administrator Homepage')
+      expect(page).to have_content('The page you were looking for doesn\'t exist.')
+
     end
   end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -11,6 +11,20 @@ describe User, type: :model do
     copy_cat = User.new(username: "user", password: "Password")
 
     expect(copy_cat).to_not be_valid
+  end
+  end
+  describe User do
+    it 'can be created as an admin' do
+      user = User.create(username: 'penelope', password: '1234', role: 1)
+
+      expect(user.role).to eq('admin')
+      expect(user.admin?).to be_truthy
+    end
+    it 'can be created as a default user' do
+      user = User.create(username: 'sammy', password: '1234', role: 0)
+
+      expect(user.role).to eq('default')
+      expect(user.default?).to be_truthy
     end
   end
 end
